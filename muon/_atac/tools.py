32d9482c83f9e17a500a02a75acc838d9bb7cedb
Danila Bredikhin <danila.bredikhin@embl.de>
2020-08-16 18:16:47 +0200

    Improve working with peak annotation and gene names
    - Move add_peak_annotation() to muon.atac.tl
    - Implement add_peak_annotation_gene_names()
    - Implement add_genes_peaks_groups()
    - Implement rank_peaks_groups() as a shorthand
      for running sc.tl.rank_genes_groups()
      and muon.atac.tl.add_genes_peaks_groups()

--- muon/_atac/tools.py
+++ muon/_atac/tools.py
@@ -2,7 +2,8 @@ import io
 import os
 from glob import glob
 import pkgutil
-from typing import Union, Optional
+from collections import OrderedDict
+from typing import Union, Optional, Callable
 import logging
 
 import numpy as np
@@ -47,6 +48,222 @@ def lsi(data: Union[AnnData, MuData], scale_embeddings=True, n_comps=50):
 	return None
 
 
+# Peak annotation
+
+def add_peak_annotation(data: Union[AnnData, MuData],
+						annotation: Union[str, pd.DataFrame],
+						sep: str = "\t",
+						return_annotation: bool = False):
+	"""
+	Parse peak annotation file and add it to the .uns["atac"]["peak_annotation"]
+
+	Parameters
+	----------
+	data
+		AnnData object with peak counts or multimodal MuData object with 'atac' modality.
+	annotation
+		A path to the peak annotation file (e.g. peak_annotation.tsv) or DataFrame with it.
+		Annotation has to contain columns: peak, gene, distance, peak_type.
+	sep
+		Separator for the peak annotation file. Only used if the file name is provided. 
+		Tab by default.
+	return_annotation
+		If return adata.uns['atac']['peak_annotation']. False by default.
+	"""
+	if isinstance(data, AnnData):
+		adata = data
+	elif isinstance(data, MuData):
+		adata = data.mod['atac']
+		# TODO: check that ATAC-seq slot is present with this name
+	else:
+		raise TypeError("Expected AnnData or MuData object with 'atac' modality")
+
+	if isinstance(annotation, str):
+		pa = pd.read_csv(annotation, sep=sep)
+	else:
+		pa = annotation
+
+	# Convert null values to empty strings
+	pa.gene[pa.gene.isnull()] = ""
+	pa.distance[pa.distance.isnull()] = ""
+	pa.peak_type[pa.peak_type.isnull()] = ""
+
+	# Split genes, distances, and peaks into individual records
+	pa_g = pd.DataFrame(pa.gene.str.split(';').tolist(), index=pa.peak).stack()
+	pa_d = pd.DataFrame(pa.distance.str.split(';').tolist(), index=pa.peak).stack()
+	pa_p = pd.DataFrame(pa.peak_type.str.split(';').tolist(), index=pa.peak).stack()
+
+	# Make a long dataframe indexed by gene
+	pa_long = pd.concat([pa_g.reset_index()[["peak", 0]],
+	                     pa_d.reset_index()[[0]],
+	                     pa_p.reset_index()[[0]]], axis=1)
+	pa_long.columns = ["peak", "gene", "distance", "peak_type"]
+	pa_long = pa_long.set_index("gene")
+
+	# chrX_NNNNN_NNNNN -> chrX:NNNNN-NNNNN
+	pa_long.peak = [peak.replace("_", ":", 1).replace("_", "-", 1) for peak in pa_long.peak]
+
+	# Make distance values nullable integers
+	# See https://pandas.pydata.org/pandas-docs/stable/user_guide/integer_na.html
+	null_distance = pa_long.distance == ""
+	pa_long.distance[null_distance] = 0
+	pa_long.distance = pa_long.distance.astype(int).astype("Int64")
+	pa_long.distance[null_distance] = np.nan
+
+	if 'atac' not in data.uns:
+		adata.uns["atac"] = OrderedDict()
+	adata.uns['atac']['peak_annotation'] = pa_long
+
+	if return_annotation:
+		return pa_long
+
+
+def add_peak_annotation_gene_names(data: Union[AnnData, MuData],
+						  		   gene_names: Optional[pd.DataFrame] = None,
+						  		   join_on: str = "gene_ids",
+						  		   return_annotation: bool = False):
+	"""
+	Add gene names to peak annotation table in .uns["atac"]["peak_annotation"]
+
+	Parameters
+	----------
+	data
+		AnnData object with peak counts or multimodal MuData object with 'atac' modality.
+	gene_names
+		A DataFrame indexed on the gene name
+	join_on
+		Name of the column in the gene_names DataFrame corresponding to the peak annotation index
+	return_annotation
+		If return adata.uns['atac']['peak_annotation']. False by default.
+	"""
+	if isinstance(data, AnnData):
+		adata = data
+	elif isinstance(data, MuData):
+		adata = data.mod['atac']
+		# TODO: check that ATAC-seq slot is present with this name
+
+		if gene_names is None:
+			if 'rna' in data.mod:
+				gene_names = data.mod['rna'].var
+			else:
+				raise ValueError("There is no .mod['rna'] modality. Provide `gene_names` as a pd.DataFrame.")
+	else:
+		raise TypeError("Expected AnnData or MuData object with 'atac' modality")
+
+	if 'atac' not in adata.uns or 'peak_annotation' not in adata.uns['atac']:
+		raise KeyError("There is no peak annotation yet. Run muon.atac.pp.add_peak_annotation first.")
+
+	# Extract a table with gene IDs and gene names only
+	gene_id_name = gene_names.loc[:,[join_on]].rename_axis("gene_name").reset_index(drop=False).set_index(join_on)
+
+	# Add gene names to the peak annotatoin table, then reset the index on gene IDs
+	ann = adata.uns['atac']['peak_annotation']
+	ann = ann.join(gene_id_name).rename_axis("gene").reset_index(drop=False)
+
+	# Use empty strings for intergenic peaks when there is no gene 
+	ann.loc[ann.gene_name.isnull(),"gene_name"] = ""
+
+	# Finally, set the index to gene name
+	ann = ann.set_index("gene_name")
+	adata.uns['atac']['peak_annotation'] = ann
+
+	if return_annotation:
+		return pa_long
+
+
+# Gene names for peaks
+def add_genes_peaks_groups(data: Union[AnnData, MuData], 
+						   peak_type: Optional[str] = None, 
+						   distance_filter: Optional[Callable[[int], bool]] = None):
+	"""
+	Add gene names to peaks ranked by clustering group
+
+	To add gene names to ranked peaks, peaks have to be ranked first.
+	For that, run `sc.tl.rank_genes_groups`.
+
+	Gene names are picked as indices of the peak annotation table.
+	To create annotation table, first run `muon.atac.tl.add_peak_annotation`.
+	To add gene names instead of gene IDs, consider 
+	running `muon.atac.tl.add_peak_annotation_gene_names` then.
+	"""
+	if isinstance(data, AnnData):
+		adata = data
+	elif isinstance(data, MuData):
+		adata = data.mod['atac']
+	else:
+		raise TypeError("Expected AnnData or MuData object with 'atac' modality")
+
+	if 'rank_genes_groups' not in adata.uns:
+		raise KeyError("There is no .uns['rank_genes_groups'] yet. Run sc.tl.rank_genes_groups first.")
+
+	if 'atac' not in adata.uns or 'peak_annotation' not in adata.uns['atac']:
+		raise KeyError("There is no peak annotation yet. Run muon.atac.pp.add_peak_annotation first.")
+
+	def choose_peak_annotations(annotation: pd.DataFrame,
+								peak: str,
+								peak_type: Optional[str] = None,
+								distance_filter: Optional[Callable[[int], bool]] = None):
+		"""
+		annotation (adata.uns['atac']['peak_annotation']), peak_type, and distance_filter
+		are fetched from the outer scope (add_genes_peaks_groups)
+		"""
+		if 'peak' not in annotation.columns:
+			raise KeyError("Peak annotation has to contain 'peak' column.")
+
+		# Choose all annotations for the peak
+		annotation = annotation[annotation.peak == peak]
+		
+		# Pick required peak types
+		if peak_type is not None:
+			if 'peak_type' not in annotation.columns:
+				raise KeyError("Peak annotation has to contain 'peak_type' column.")
+			annotation[annotation.peak_type == peak_type]
+
+		# Pick annotations at required distance
+		if distance_filter is not None:
+			if 'distance' not in annotation.columns:
+				raise KeyError("Peak annotation has to contain 'distance' column.")
+			annotation = annotation[distance_filter(annotation.distance)]
+
+		return annotation
+
+	annotation = adata.uns['atac']['peak_annotation']
+
+	adata.uns['rank_genes_groups']['genes'] = {}
+	for i in adata.uns['rank_genes_groups']['names'].dtype.names:
+	    group = adata.uns['rank_genes_groups']['names'][i]
+	    genes = [', '.join(choose_peak_annotations(annotation, value, peak_type, distance_filter).index.values) for value in group]
+	    adata.uns['rank_genes_groups']['genes'][i] = genes
+
+	# Convert to rec.array to match 'names', 'scores', and 'pvals'
+	adata.uns['rank_genes_groups']['genes'] = pd.DataFrame(adata.uns['rank_genes_groups']['genes']).to_records()
+
+
+def rank_peaks_groups(data: Union[AnnData, MuData], **kwargs):
+	"""
+	Rank peaks in clusters groups.
+
+	Shorthand for running sc.tl.rank_genes_groups
+	followed by muon.atac.tl.add_genes_peaks_groups.
+
+	See sc.tl.rank_genes_groups for details.
+	"""
+
+	if isinstance(data, AnnData):
+		adata = data
+	elif isinstance(data, MuData):
+		adata = data.mod['atac']
+	else:
+		raise TypeError("Expected AnnData or MuData object with 'atac' modality")
+
+	sc.tl.rank_genes_groups(adata, **kwargs)
+
+	add_genes_peaks_groups(adata)
+
+
+
+# Sequences and motifs
+
 def _parse_motif_ids(filename: Optional[str] = None):
 	if filename is None:
 		# Use a file from the embedded JASPAR database
@@ -79,7 +296,7 @@ def _parse_motif_matrices(files: Optional[str] = None,
 	return {'motifs': [os.path.basename(f).rstrip(".pfm") for f in files],
 			'matrices': matrices}
 
-def _prepare_motif_scanner(matrices,
+def _prepare_motif_scanner(matrices = None,
 						   background: int = 4,
 						   pvalue: float = 0.0001,
 						   max_hits: int = 10):
@@ -119,8 +336,8 @@ def scan_sequences(sequences,
 			"MOODS is not available. Install MOODS from PyPI (`pip install MOODS-python`) or from GitHub (`pip install git+https://github.com/jhkorhonen/MOODS`)"
 			)
 
-	if matrices is None or motifs is None:
-		assert matrices is None and motifs is None, "Both a list of matrices and a corresponding list of motif IDs should be provided — or none to use the built-in ones."
+	if motifs is None:
+		assert matrices is None, "Both a list of matrices and a corresponding list of motif IDs should be provided — or none to use the built-in ones, unless a scanner is provided."
 
 	if motif_scanner is None:
 		if matrices is None:
