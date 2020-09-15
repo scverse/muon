49031906fc90efba547960ea151867007625bb73
Danila Bredikhin <danila.bredikhin@embl.de>
2020-09-15 20:12:13 +0200

    Add a simple test for reading h5mu with file backing

--- tests/test_io.py
+++ tests/test_io.py
@@ -4,6 +4,7 @@ import pytest
 import os
 
 import numpy as np
+from scipy.sparse import csr_matrix
 from anndata import AnnData
 import muon as mu
 from muon import MuData
@@ -21,6 +22,17 @@ class TestMuData():
 		assert list(mdata_.mod.keys()) == ['mod1', 'mod2']
 		assert mdata.mod['mod1'].X[51,9] == pytest.approx(51.9)
 		assert mdata.mod['mod2'].X[42,18] == pytest.approx(959)
+
+@pytest.mark.usefixtures('filepath_h5mu')
+class TestMuDataMod():
+	def test_h5mu_mod_backed(self, mdata, filepath_h5mu):
+		mdata.write(filepath_h5mu,)
+		mdata_ = mu.read_h5mu(filepath_h5mu, backed='r')
+		assert list(mdata_.mod.keys()) == ['mod1', 'mod2']
+
+		# When backed, the matrix is read-only
+		with pytest.raises(OSError):
+			mdata_.mod['mod1'].X[10,5] = 0
 
 if __name__ == "__main__":
 	unittest.main()
