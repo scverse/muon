22e612a029b4ce5e26892e769053a4a14eac2f1a
Max Frank <max.florian.frank@gmail.com>
2020-10-07 14:45:41 +0200

    Made get_gene_annotation accept AnnData or MuData

--- muon/_core/utils.py
+++ muon/_core/utils.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from .._core.mudata import MuData
+from anndata import AnnData
+from typing import Union
 
 
-def get_gene_annotation_from_rna(mdata: MuData):
+def get_gene_annotation_from_rna(data: Union[AnnData, MuData]):
     """
 	Get data frame with start and end positions from interval
 	column of the 'rna' layers .var.
@@ -13,23 +15,28 @@ def get_gene_annotation_from_rna(mdata: MuData):
 		MuData object
 	"""
 
-    if "rna" in mdata.mod:
-        if "interval" in mdata.mod["rna"].var.columns:
-            features = pd.DataFrame(
-                [
-                    s.replace(":", "-", 1).split("-")
-                    for s in mdata.mod["rna"].var.interval
-                ]
-            )
-            features.columns = ["Chromosome", "Start", "End"]
-            features["gene_id"] = mdata.mod["rna"].var.gene_ids.values
-            features["gene_name"] = mdata.mod["rna"].var.index.values
-            # Remove genes with no coordinates indicated
-            features = features.loc[~features.Start.isnull()]
-            features.Start = features.Start.astype(int)
-            features.End = features.End.astype(int)
-        else:
-            raise ValueError(".var object does not have a column named interval")
+    if isinstance(data, AnnData):
+        adata = data
+    elif isinstance(data, MuData) and 'rna' in data.mod:
+        adata = data.mod['rna']
     else:
-        raise ValueError("No rna layer found")
+        raise TypeError("Expected AnnData or MuData object with 'rna' modality")
+
+
+    if "interval" in adata.var.columns:
+        features = pd.DataFrame(
+            [
+                s.replace(":", "-", 1).split("-")
+                for s in adata.var.interval
+            ]
+        )
+        features.columns = ["Chromosome", "Start", "End"]
+        features["gene_id"] = adata.var.gene_ids.values
+        features["gene_name"] = adata.var.index.values
+        # Remove genes with no coordinates indicated
+        features = features.loc[~features.Start.isnull()]
+        features.Start = features.Start.astype(int)
+        features.End = features.End.astype(int)
+    else:
+        raise ValueError(".var object does not have a column named interval")
     return features
