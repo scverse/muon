4ffca5e9946db6bc0bd51cd33f011823823b6543
Max Frank <max.florian.frank@gmail.com>
2020-11-13 12:22:26 +0100

    Allow underscore in region strings

--- muon/_atac/utils.py
+++ muon/_atac/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def parse_region_string(region: str):
-    feat_list = re.split("-|:", region)
+    feat_list = re.split("_|-|:", region)
     feature_df = pd.DataFrame(columns=["Chromosome", "Start", "End"])
     feature_df.loc[0] = feat_list
     feature_df = feature_df.astype({"Start": int, "End": int})
