f97aaebbf4317b438646229a82d77fde424c48d7
Danila <32863903+gtca@users.noreply.github.com>
2021-01-13 01:48:34 +0100

    Add more trove classifiers to setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
     ],
     python_requires=">=3.6",
     install_requires=[
