df73b3b2f7dba9c352026ac15b1815113adb3d36
Ilia Kats <ilia-kats@gmx.net>
2021-04-13 13:40:00 +0200

    roll our own implementation of _make_index_unique
    AnnData's implementation attempts to construct unique strings, so if a
    proposed name is already present in the index, the counter is
    incremented until it finds an unoccupied name. For our usecase, this may
    lead to wrong joins under certain conditions.

--- muon/_core/utils.py
+++ muon/_core/utils.py
@@ -1,9 +1,20 @@
+from collections import Counter
 import pandas as pd
+import numpy as np
 from anndata.utils import make_index_unique
 
 
 def _make_index_unique(df: pd.DataFrame) -> pd.DataFrame:
-    return df.set_index(make_index_unique(df.index), append=True)
+    dup_idx = np.zeros((df.shape[0],), dtype=np.uint8)
+    if not df.index.is_unique:
+        duplicates = np.nonzero(df.index.duplicated())[0]
+        cnt = Counter()
+        for dup in duplicates:
+            idxval = df.index[dup]
+            newval = cnt[idxval] + 1
+            dup_idx[dup] = newval
+            cnt[idxval] = newval
+    return df.set_index(dup_idx, append=True)
 
 
 def _restore_index(df: pd.DataFrame) -> pd.DataFrame:
