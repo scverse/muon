e719e505489e3b9cda72505ed32dd09fbfbcf05b
Danila Bredikhin <danila.bredikhin@embl.de>
2021-04-26 23:57:37 +0200

    Add tests for mu.pp.filter_var()

--- tests/test_muon_preproc.py
+++ tests/test_muon_preproc.py
@@ -22,6 +22,9 @@ def mdata():
 
 @pytest.mark.usefixtures("filepath_h5mu")
 class TestInPlaceFiltering:
+
+    # Observations
+
     def test_filter_obs_adata(self, mdata, filepath_h5mu):
         ad = mdata["mod1"].copy()
         sub = np.random.binomial(1, 0.5, ad.n_obs).astype(bool)
@@ -55,6 +58,42 @@ class TestInPlaceFiltering:
         with pytest.raises(ValueError):
             sub = np.random.binomial(1, 0.5, view.n_obs).astype(bool)
             mu.pp.filter_obs(view, sub)
+
+    # Variables
+
+    def test_filter_var_adata(self, mdata, filepath_h5mu):
+        ad = mdata["mod1"].copy()
+        sub = np.random.binomial(1, 0.5, ad.n_vars).astype(bool)
+        mu.pp.filter_var(ad, sub)
+        assert ad.n_vars == sub.sum()
+
+    def test_filter_var_mdata(self, mdata, filepath_h5mu):
+        md = mdata.copy()
+        sub = np.random.binomial(1, 0.5, md.n_vars).astype(bool)
+        mu.pp.filter_var(md, sub)
+        assert md.n_vars == sub.sum()
+        assert md["mod1"].n_vars == mdata.varm["mod1"][sub].sum()
+        assert md["mod2"].n_vars == mdata.varm["mod2"][sub].sum()
+
+    def test_filter_var_adata_backed(self, mdata, filepath_h5mu):
+        mdata.write(filepath_h5mu)
+        mdata_ = mu.read_h5mu(filepath_h5mu, backed="r")
+        assert list(mdata_.mod.keys()) == ["mod1", "mod2"]
+
+        # When backed, in-place filtering should throw a warning
+        with pytest.warns(UserWarning):
+            sub = np.random.binomial(1, 0.5, mdata_.mod["mod1"].n_vars).astype(bool)
+            print("Sub:\t", len(sub))
+            print("Size:\t", mdata_.mod["mod1"].n_vars)
+            mu.pp.filter_var(mdata_.mod["mod1"], sub)
+
+    def test_filter_var_adata_view(self, mdata, filepath_h5mu):
+        pov = np.random.binomial(1, 0.4, mdata.mod["mod1"].n_obs).astype(bool)
+        view = mdata.mod["mod1"][pov, :]
+        # When backed, in-place filtering should throw an error
+        with pytest.raises(ValueError):
+            sub = np.random.binomial(1, 0.5, view.n_var).astype(bool)
+            mu.pp.filter_var(view, sub)
 
 
 if __name__ == "__main__":
