85fcfe554740c080cc29204d01fa8a7bcf294025
Danila Bredikhin <danila.bredikhin@embl.de>
2021-09-24 18:18:02 +0200

    Update config.se_options

--- muon/_core/config.py
+++ muon/_core/config.py
@@ -1,15 +1,9 @@
 from mudata._core import config as mudata_config
 import logging as log
 
-OPTIONS = {
-    "display_style": "text",
-    "display_html_expand": 0b010,
-}
+OPTIONS = {}
 
-_VALID_OPTIONS = {
-    "display_style": lambda x: x in ("text", "html"),
-    "display_html_expand": lambda x: isinstance(x, int) and len(bin(x or 0b111)) == 5,
-}
+_VALID_OPTIONS = {}
 
 
 class set_options:
@@ -36,7 +30,7 @@ class set_options:
 
     def __init__(self, **kwargs):
         self.opts = {}
-        mudata_opts = {}
+        self.mudata_opts = {}
         for k, v in kwargs.items():
             if k not in OPTIONS and k not in mudata_config.OPTIONS:
                 raise ValueError(f"There is no option '{k}' available")
@@ -47,18 +41,20 @@ class set_options:
                 self.opts[k] = OPTIONS[k]
             else:
                 # For mudata options, there validity is going to be checked by mudata
-                mudata_opts[k] = mudata_config.OPTIONS[k]
-        mudata_config.set_options(**mudata_opts)
-        # Muon options take precedence
-        self._apply(kwargs)
+                self.mudata_opts[k] = mudata_config.OPTIONS[k]
+        mudata_config.set_options(**{k: v for k, v in kwargs.items() if k in mudata_config.OPTIONS})
+        if self.opts:
+            self._apply(**{k: v for k, v in kwargs.items() if k in OPTIONS})
 
     def _apply(self, opts):
         OPTIONS.update(opts)
 
     def __enter__(self):
         log.info("Using custom muon options in the new context...")
-        return OPTIONS
+        return {**mudata_config.OPTIONS, **OPTIONS}
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        log.info(f"Returning to the previously defined options: {self.opts}")
+        log.info(f"Returning to the previously defined mudata options: {self.mudata_opts}")
+        log.info(f"Returning to the previously defined muon options: {self.opts}")
+        mudata_config.set_options(**self.mudata_opts)
         self._apply(self.opts)
