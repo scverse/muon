21881a182f8bce97f067b32cbdd131eba748f617
Danila Bredikhin <danila.bredikhin@embl.de>
2022-02-17 22:27:30 +0100

    Use sphinx_book_theme for documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath("../"))
 # -- Project information -----------------------------------------------------
 
 project = "muon"
-copyright = "2020 - 2021, Danila Bredikhin"
+copyright = "2020 - 2022, Danila Bredikhin"
 author = "Danila Bredikhin"
 
 
@@ -39,7 +39,6 @@ extensions = [
     "sphinx_automodapi.automodapi",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.mathjax",
-    "sphinx_rtd_theme",
     "nbsphinx",
 ]
 autosectionlabel_prefix_document = True
@@ -67,9 +66,7 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-# html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
