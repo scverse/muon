74cf7a8112397fcb9a02acf53aa821e0fa50e7b3
ilan-gold <ilanbassgold@gmail.com>
2024-10-15 12:01:34 +0200

    (chore): fixture

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,12 +1,10 @@
-import scanpy as sc
 import muon as mu
 
 class TestFilter():
-    def test_filter_obs_simple(self):
-        adata = sc.datasets.pbmc3k_processed()
+    def test_filter_obs_simple(self, pbmc3k_processed):
         mdata = mu.MuData({
-            "A": adata[:500, ].copy(),
-            "B": adata[500:, ].copy()
+            "A": pbmc3k_processed[:500, ].copy(),
+            "B": pbmc3k_processed[500:, ].copy()
         })
         mu.pp.filter_obs(mdata, "A:louvain", lambda x: x == "B cells")
         assert mdata["B"].n_obs == 0
