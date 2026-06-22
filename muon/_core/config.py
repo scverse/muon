5531c338e8e2b0509b6e81fa51de774f598e5c80
Ilia Kats <ilia-kats@gmx.net>
2026-06-22 16:59:34 +0200

    settings: don't forward settings to mudata
    incompatible with the upcoming mudata 0.4

--- muon/_core/config.py
+++ muon/_core/config.py
@@ -1,4 +1,3 @@
-from mudata._core import config as mudata_config
 import logging as log
 
 OPTIONS = {}
@@ -30,19 +29,13 @@ class set_options:
 
     def __init__(self, **kwargs):
         self.opts = {}
-        self.mudata_opts = {}
         for k, v in kwargs.items():
-            if k not in OPTIONS and k not in mudata_config.OPTIONS:
+            if k not in OPTIONS:
                 raise ValueError(f"There is no option '{k}' available")
             if k in _VALID_OPTIONS:
                 if not _VALID_OPTIONS[k](v):
                     raise ValueError(f"Value '{v}' for the option '{k}' is invalid.")
-            if k in OPTIONS:
-                self.opts[k] = OPTIONS[k]
-            else:
-                # For mudata options, there validity is going to be checked by mudata
-                self.mudata_opts[k] = mudata_config.OPTIONS[k]
-        mudata_config.set_options(**{k: v for k, v in kwargs.items() if k in mudata_config.OPTIONS})
+            self.opts[k] = OPTIONS[k]
         if self.opts:
             self._apply(**{k: v for k, v in kwargs.items() if k in OPTIONS})
 
@@ -51,10 +44,8 @@ class set_options:
 
     def __enter__(self):
         log.info("Using custom muon options in the new context...")
-        return {**mudata_config.OPTIONS, **OPTIONS}
+        return {**OPTIONS}
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        log.info(f"Returning to the previously defined mudata options: {self.mudata_opts}")
         log.info(f"Returning to the previously defined muon options: {self.opts}")
-        mudata_config.set_options(**self.mudata_opts)
         self._apply(self.opts)
